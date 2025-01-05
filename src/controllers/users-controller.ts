e5d04fc9c4bb5e160099dd26c81b948bf5cd5e9c
styven <styvenn16@gmail.com>
2025-01-05 10:19:13 -0300

    Validando Dados do Usuário

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,6 +1,14 @@
 import {Request, Response} from "express"
+import z from "zod"
+
 class UsersController{
     create(request: Request, response:Response){
+        const bodySchema = z.object({
+            name: z.string().trim().min(3),
+            email: z.string().email(),
+            password: z.string().min(3)
+        })
+        const {name, email, password} = bodySchema.parse(request.body)
         return response.json({message: "ok"})
     }
 }
