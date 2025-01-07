5b1cc26f8a7355dca0ce378e92b238d0c888a638
styven <styvenn16@gmail.com>
2025-01-07 06:57:30 -0300

    cadastro de tarefas

--- src/controllers/tasks-controller.ts
+++ src/controllers/tasks-controller.ts
@@ -9,6 +9,22 @@ import {sign} from "jsonwebtoken"
 
 export class TasksController{
     async create(request: Request, response:Response){
-        return response.json({message:"tarefas"})
+        const bodySchema = z.object({
+            title: z.string(),
+            description: z.string(),
+            assigned_id: z.number(),
+            team_id: z.number()
+        })
+        const { title,description,assigned_id,team_id} = bodySchema.parse(request.body)
+
+        await prisma.tasks.create({
+            data:{
+                title:title,
+                description:description,
+                assignedTo: assigned_id,
+                teamId: team_id
+            }
+        })
+        return response.status(201).json()
     }
 }
